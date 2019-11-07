e705040eaefe69162220b7d4e3ac51d411cfeb86
Vladimir Tyrin <b0-0b@yandex.ru>
2019-11-07 17:58:15 +0300

    updated output

--- SudokuSolver.Console/Program.cs
+++ SudokuSolver.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using SudokuSolver.Engine;
 using SudokuSolver.Engine.InitialState;
 
@@ -6,21 +7,51 @@ namespace SudokuSolver.Console
 {
     internal static class Program
     {
+        private const int PrintEverySteps = 1;
+
         private static void Main()
         {
-            var initialStateProvider = new EmptyInitialStateProvider();
+            var initialStateProvider = GetInitialStateProvider();
 
             var game = GameFactory.Create(initialStateProvider);
 
             game.Step += GameOnStep;
+            game.GameCompleted += GameOnGameCompleted;
 
             game.Run();
         }
 
+        private static IInitialStateProvider GetInitialStateProvider()
+        {
+            var state = new int[Constants.FieldSize, Constants.FieldSize];
+
+            state[0, 0] = 1;
+            //state[0, 1] = 2;
+            //state[0, 2] = 3;
+
+            return new FixedInitialStateProvider(state);
+        }
+
         private static void GameOnStep(object sender, FieldState e)
         {
+            if (e.AlgorithmStep % PrintEverySteps != 0)
+                return;
+            DrawState(e);
+        }
+
+        private static void GameOnGameCompleted(object sender, FieldState e)
+        {
+            var prevColor = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine("Completed");
+            DrawState(e);
+            System.Console.ForegroundColor = prevColor;
+        }
+
+        private static void DrawState(FieldState e)
+        {
             var builder = new StringBuilder(120);
-            builder.AppendLine($"{e.AlgorithmStep} - {e.FilledCellsCount}");
+            builder.AppendLine($"Step {e.AlgorithmStep} - filled {e.FilledCellsCount}");
             for (var i = 0; i < Constants.FieldSize; i++)
             {
                 for (var j = 0; j < Constants.FieldSize; j++)
---
+++ SudokuSolver.Engine/InitialState/FixedInitialStateProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace SudokuSolver.Engine.InitialState
+{
+    public class FixedInitialStateProvider : IInitialStateProvider
+    {
+        private readonly int[,] _state;
+
+        public FixedInitialStateProvider(int[,] state)
+        {
+            _state = state;
+        }
+
+        public InitialFieldState GetInitialFieldState()
+        {
+            return new InitialFieldState(_state);
+        }
+    }
+}
